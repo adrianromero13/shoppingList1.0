c1a667b0433381ecdc4ea70166be951f37471eb1
Adrian Romero <radrian031613@gmail.com>
2021-07-06 20:48:22 -0700

    set up the navbar with buttons and styling using @material-ui styling props

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import Switch from '@material-ui/core/Switch';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
+
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+import NavButtons from '../NavBarButtons';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -63,7 +68,9 @@ export default function NavBar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            Lists
+            
+            <NavButtons />
+
           </Typography>
           {auth && (
             <div>
