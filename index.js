869e4d3070d8170b8c8691f2c4c9bf9d5ad1209b
Adrian Romero <radrian031613@gmail.com>
2021-07-24 13:32:56 -0700

    installed serve to attempt fix on heroku deploy

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
+// ommited script
+// "scripts": {
+//   "start": "if-env NODE_ENV=production && npm run start:prod || npm run start:dev",
+//   "start:prod": "node index.js",
+//   "start:dev": "concurrently \"nodemon --ignore 'client/*'\" \"npm run client\"",
+//   "client": "cd client && npm run start",
+//   "install": "cd client && npm install",
+//   "build": "cd client && npm run build",
+//   "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
+// },
+
 app.use(routes);
 require('./services/passport');
 // Connect database
